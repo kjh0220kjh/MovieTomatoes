7f3a0ebc684f0e13707ca493e109ad0e4d83bd5f
kjh0220kjh <kjh0220kjh@naver.com>
2021-11-23 16:18:09 +0900

    train/test 데이터 셋 Open

--- AI/ModelLearning.py
+++ AI/ModelLearning.py
@@ -3,7 +3,7 @@
 # >> Module: Tensorflow, Keras
 # >> Dataset: Naver Sentiment Movie Corpus
 
-dataset Intro
+
 
 
 # 데이터셋: Nave Sentiment Movie Corpus(https://github.com/e9t/nsmc/)
@@ -18,3 +18,71 @@ dataset Intro
 # >> document: 리뷰의 내용
 # >> label: 긍정(1)인지 부정(0)인지 나타냄
 #           평점이 긍정(9~10졈), 부정(1~4점), 5~8점은 제거
+
+import json
+import os
+import nltk
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib import font_manager, rc
+from pprint import pprint
+from konlpy.tag import Okt
+from tensorflow.keras import models
+from tensorflow.keras import layers
+from tensorflow.keras import optimizers
+from tensorflow.keras import losses
+from tensorflow.keras import metrics
+
+#############
+# File Open #
+#############
+
+# .txt 파일에서 데이터를 불러오는 method
+def read_data(filename):
+    with open(filename, 'r', encoding='URF-8') as f:
+        data = [line.split('\t') for line in f.read().splitlines()]
+
+
+        data = []
+        for line in f.read().splitlines():
+            # line = 9976970 \t	아 더빙.. 진짜 짜증나네요 목소리 \t 0
+            # line,split('\t') [9976970, 아 더빙.. 진짜 짜증나네요 목소리, 0]
+            data.append(line,split('\t'))
+
+
+
+        data = data[1:]  # 제목열 제외
+    return data
+
+# nsmc 데이터를 불러와서 python 변수에 담기
+train_data = read_data('./dataset/ratings_train.txt')  # 트레이닝 데이터 Open
+test_data = read_data('./dataset/ratings_test.txt')
+
+'C:/cnu_workspace/MovieTomatoes/as/dataset/ratings_train.txt'  # 절대경로
+
+# / - 하위폴더
+# .. - 상위폴더
+# . - 현재폴더
+
+./datset/ratings_train.txt
+
+
+# 절대경로와 상대경로
+# C:/cnu_workspace/MovieTomatoes
+#                   ㄴ ai
+#                       ㄴ dataset
+#                           ㄴ ratings_text.txt
+#                           ㄴ ratings_train.txt
+#                   ㄴ ModelLearning.py
+#                   ㄴ model
+#                   ㄴ webcrawl
+#                   ㄴ main.py
+#                   ㄴ README.md
+
+
+
+print(len(train_data))
+print(train_data[0])
+
+print(len(test_data))
+print(test_data[0])
